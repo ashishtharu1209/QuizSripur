4daba9f37a22786de1a376ea45068a2c5a607d89
ashishtharu1209 <ashishk077@academiacollege.edu.np>
2024-04-29 21:40:37 +0545

    new

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 const Login = () => {
   return (
    <>
-    <div className=' h-screen flex flex-col items-center md:flex-row md:justify-evenly login-page'>
-     <div className='text-box'>
-            <h1 className='text-blue-600 text-8xl font-bold px-12'>  SripurQuiz </h1>
-         <h4 className='text-2xl px-8 text-black font-bold'>
+    <div className=' h-screen flex flex-col items-center md:flex-row md:justify-evenly '>
+    <div className='text-box py-16 pl-20 '>
+            <h1 className='text-blue-600 text-8xl font-bold px-12'> ShripurQuiz </h1>
+         <h4 className='text-2xl px-12 text-black font-bold'>
        Expand Your Mind, One Question at a Time!
                 </h4>
                 </div>
-                <div className='w-10/12 h-auto sm:w-7/12 md:w-5/12 lg:w-3/12 xl:w-3/12 login-form'>
-                <form className='w-full h-min bg-white shadow-lg rounded-lg mt-6 backdrop-contrast-50 bg-white/30 ...'>
+                <div className='w-10/12 h-auto sm:w-7/12 md:w-5/12 lg:w-3/12 xl:w-3/12'>
+                <form className='w-full h-min  bg-white shadow-lg rounded-lg mt-6 backdrop-contrast-50 bg-white/30 ...'>
                 <input
 type="email"
 placeholder='email or phone number'
@@ -25,7 +25,8 @@ className='w-11/12 h-14 p-5 border-2 border-solid m-3 border-gray-100 rounded-lg
 Login
 </button>
 <hr className='mt-4 w-4/5 mx-auto' />
-<button className=' w-11/12 h-11 m-3 bg-green-600 text-white my-8  text-2xl font-semibold rounded-lg'>Sign Up
+<p className='text-black mt-3 text-2xl mx-12 text-center font-semibold'>Don't Have Account?</p>
+<button className=' w-11/12 h-12 m-3 bg-green-600 text-white my-8 text-2xl font-semibold rounded-lg content-center'>Sign Up
 </button>
 </form>
 </div>
